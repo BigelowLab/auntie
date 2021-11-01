1fe1ff038da5aa974da0c473795c7af27c474e22
Ben Tupper <btupper@bigelow.org>
2021-11-01 16:49:21 -0400

    update README, augment inst/scripts/test_subsample.R

--- inst/scripts/test_resample.R
+++ inst/scripts/test_resample.R
@@ -4,7 +4,8 @@ root <- "/mnt/storage/data/edna/mothur/projects/subsample"
 filepairs = auntie::list_filepairs(file.path(root, "raw"))
 default_path <- file.path(root, "test_sub")
 charlier::make_path(default_path)
-ok <- auntie::bbmap_subsample(filepairs, path = default_path)
+ok <- auntie::bbmap_subsample(filepairs, path = default_path, 
+  reformat_args = "ow=f samplereadstarget=10000 sampleseed=7")
 
 fp <- auntie::list_filepairs(default_path)
 ids <- auntie::fastq_ids(fp)
